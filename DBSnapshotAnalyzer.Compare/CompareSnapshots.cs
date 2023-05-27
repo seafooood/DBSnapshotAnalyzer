7163a496cf56d9d7a3da88a72a36ecbee0fec57b
Andrew Seaford <mail@aseaford.co.uk>
2023-05-27 02:33:12 +0100

    initial commit of the table comparison

---
+++ DBSnapshotAnalyzer.Compare/CompareSnapshots.cs
@@ -0,0 +1,31 @@
+﻿using DiffPlex.DiffBuilder;
+using DiffPlex;
+using DiffPlex.DiffBuilder.Model;
+
+namespace DBSnapshotAnalyzer.Compare
+{
+    public class CompareSnapshots
+    {
+        public List<string> CompareTables(string tableContent1, string tableContent2)
+        {
+            var diffBuilder = new InlineDiffBuilder(new Differ());
+            var diff = diffBuilder.BuildDiffModel(tableContent1, tableContent2);
+
+            var result = new List<string>();
+            foreach (var line in diff.Lines)
+            {
+                switch (line.Type)
+                {
+                    case ChangeType.Inserted:
+                        result.Add($"+{line.Text}");
+                        break;
+                    case ChangeType.Deleted:
+                        result.Add($"-{line.Text}");
+                        break;
+                }
+            }
+
+            return result;
+        }
+    }
+}

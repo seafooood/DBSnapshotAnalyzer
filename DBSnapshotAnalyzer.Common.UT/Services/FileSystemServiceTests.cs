9db69d0b853bd959c3e68c795e9f50226b8b4a9d
Andrew Seaford <mail@aseaford.co.uk>
2023-05-30 22:43:43 +0100

    Added GetTableNameFromFileName test

--- DBSnapshotAnalyzer.Common.UT/Services/FileSystemServiceTests.cs
+++ DBSnapshotAnalyzer.Common.UT/Services/FileSystemServiceTests.cs
@@ -1,4 +1,7 @@
-﻿using System.Diagnostics.CodeAnalysis;
+﻿using DBSnapshotAnalyzer.Common.Services;
+using Moq;
+using NLog;
+using System.Diagnostics.CodeAnalysis;
 
 namespace DBSnapshotAnalyzer.Common.UT.Services
 {
@@ -6,5 +9,25 @@ namespace DBSnapshotAnalyzer.Common.UT.Services
     [ExcludeFromCodeCoverage]
     public class FileSystemServiceTests
     {
+        #region GetTableNameFromFileName Tests
+        /// <summary>
+        /// Test confirms that GetTableNameFromFileName can convert a file name to table name
+        /// </summary>
+        /// <param name="name"></param>
+        [TestCase("bob")]
+        [TestCase("name")]
+        public void GetTableNameFromFileNameTest(string name)
+        {
+            // Arrange
+            var mockLogger = new Mock<ILogger>();
+            var systemUnderTest = new FileSystemService(mockLogger.Object);
+
+            // Act
+            var result = systemUnderTest.GetTableNameFromFileName(name+".csv");
+
+            // Assert
+            Assert.That(result, Is.EqualTo(name)); // Confirm the table name 
+        }
+        #endregion
     }
 }
